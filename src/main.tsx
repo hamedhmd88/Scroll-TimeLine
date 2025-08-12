fb20215ccb9163f3084a6eb341ff073540903e93
Hamed Mahjoobi <hmdhamed88@gmail.com>
2025-08-12 19:21:38 +0330

    add result for firefox

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';  // ایمپورت مستقیم از URL برای پلی‌فیل
+// برای هندل کردن اسکرول و ویو که در سی اسس گفته شده برای مرورگر فایرفاکس که اجرا کنه 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
